c300f764530ad841164aebd5aa3e922a25f6bdd2
matsueushi <sinxcosxtanx@gmail.com>
2018-07-05 22:11:10 -0400

    保存ファイル名の修正

--- discogs_xml_audacity.py
+++ discogs_xml_audacity.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import discogs_client
 import re
 import os
@@ -11,6 +12,10 @@ SAVE_PATH = os.getenv('SAVE_PATH')
 USER_TOKEN = os.getenv('USER_TOKEN')
 
 d = discogs_client.Client('AudacityTagDiscogs/0.1', user_token=USER_TOKEN)
+
+
+def trim_artist_name(name):
+    return re.sub(' \(\d\)$', '', name)
 
 
 def audacity_tag(name, val):
@@ -31,7 +36,7 @@ def discogs_info_toxml(release):
     disc_info_dic['YEAR'] = release.year
     disc_info_dic['GENRE'] = release.genres[0]
     # remove " (*)" of "artist name (*)"
-    disc_info_dic['ARTIST'] = re.sub(' \(\d\)$', '', release.artists[0].name)
+    disc_info_dic['ARTIST'] = trim_artist_name(release.artists[0].name)
     disc_info_dic['ALBUM'] = release.title
 
     for i, t in enumerate(release.tracklist):
@@ -45,7 +50,7 @@ def discogs_info_toxml(release):
 
         # print('%s - %02d %s.xml' % (release.title, i, t.title))
         # print(disc_info_xml.toprettyxml())
-        file_name = '%s - %02d %s.xml' % (release.title, i, t.title)
+        file_name = '%s - %02d %s.xml' % (release.title, i + 1, t.title)
         file_name = re.sub('/', '_', file_name)
         info[file_name] = disc_info_xml
     return info
@@ -54,21 +59,21 @@ def discogs_info_toxml(release):
 def download_album_info(discogs_id):
     release = d.release(discogs_id)
     image_url = release.images[0]['uri']
+    artist_name = trim_artist_name(release.artists[0].name)
+    sub_save_path = os.path.join(SAVE_PATH, artist_name)
+    if not os.path.exists(sub_save_path):
+        os.makedirs(sub_save_path)
 
     for file_name, x in discogs_info_toxml(release).items():
         xml_string = x.toprettyxml()
-        with open(os.path.join(SAVE_PATH, file_name), 'w') as f:
+        with open(os.path.join(sub_save_path, file_name), 'w') as f:
             f.write(xml_string)
 
     try:
         image_name = '%s.jpg' % release.title
         urllib.request.urlretrieve(
-            image_url, os.path.join(SAVE_PATH, image_name))
+            image_url, os.path.join(sub_save_path, image_name))
     except:
         sys.exit('Unable to download image')
 
     print('complete!')
-
-
-discogs_id = 1147938
-download_album_info(discogs_id)
---
+++ main.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import discogs_xml_audacity as dxa
+
+discogs_id = 442406
+dxa.download_album_info(discogs_id)
